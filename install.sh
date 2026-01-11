8cd190f79d6b9e01e87e73d67b22c32150495adb
milkarmelowa <emiliapliszka@onet.pl>
2026-01-11 22:29:10 +0100

    Add python-pywal and waypaper to install script

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
-sudo pacman -S hyprland wofi fastfetch thunar nvim kitty swww waybar firefox ttf-jetbrains-mono-nerd otf-font-awesome
+sudo pacman -S hyprland wofi fastfetch thunar nvim kitty swww waybar firefox ttf-jetbrains-mono-nerd otf-font-awesome python-pywal
 yay -S waypaper nm-applet
